337201397c3436ff7623a52d88ab62061536998e
ronaldogdm <ronaldogarciadem@gmail.com>
2019-10-28 00:08:30 -0400

    view

--- a07/view.js
+++ a07/view.js
@@ -1,145 +1,173 @@
 import Game from "./engine/game.js";
 
-var createdListeners = false;
+function gameview() {
+    return $(`
+    <div id="root">
+        <section id="header">
+            <div class="header-text has-text-weight-semibold">2048</div>
+        </section>
 
-export const updateBoard = function(game){
-    document.getElementById("score").innerHTML = "Score: " + game.score;
-    if(game.over == true){
-        document.getElementById("end").innerHTML = "You lost, your score was " + game.score + ". Press <strong>New Game</strong> to try again!";
-    }
-    for(var i = 0; i < game.board.length; i++){
-        document.getElementById(i.toString(10)).innerHTML = game.board[i];
-        if(game.board[i] == 0){
-            $(`#${i.toString(10)}`).css("visibility", "hidden"); 
-        } else if (game.board[i] == 2048) {
-            document.getElementById("end").innerHTML = "You won! Keep going!";
-        } else {
-            $(`#${i.toString(10)}`).css("visibility", "visible"); 
+        <section id="game-view">
+            <div id="scoreboard">
+                <div class="container">
+                    <div class="button has-text-weight-semibold" id="score-panel" disabled>
+                        <p>Score: </p>
+                        <p id="score"></p>
+                    </div>
+                    <button class="button is-white has-text-weight-semibold" id="newgame-button">New Game</button>
+                </div>
+            </div>
+            
+            <div id="game-pro">
+                <div id="game-board"></div>
+            </div>
+            <div id="game-message">
+                <p> <br>
+                    Join the numbers and get to the 2048 tile! <br><br>
+                    2048 implemented in grayscale for players with a visual impairment.
+                </p>
+            </div>
+        </section>
+
+        <section id="credit">
+            Adam Alston, 2019
+        </section>
+    </div>
+    `);
+}
+
+function loadGame(game) {
+    let size = game.size;
+    // Generate empty tiles
+    for (let i = 0; i < size; i++) {
+        for (let j = 0; j < size; j++) {
+            $('#game-board').append(
+                $('<div class="game-tile empty-tile"></div>')
+                .css({top: ((1 + i * 15)+'vmin'), left: ((1 + j * 15)+'vmin')})
+            );
         }
-        colorTiles(game, i);
+    }
+
+    loadScore(game);
+    loadTiles(game);
+}
+
+function loadScore(game) {
+    $('#score').empty().text(game.gameState.score);
+}
+
+function loadTiles(game) {
+    let size = game.size;
+    let board = game.gameState.board;
+    let gameboard = $('#game-board');
+
+    for (let i = 0; i < size; i++) {
+        for (let j = 0; j < size; j++) {
+            gameboard.find('#' + (i * size + j)).remove();
+
+            let currentValue = board[i * size + j];
+            if (currentValue != 0) {
+                gameboard.append(
+                    $(`<div class="game-tile centerall tile-${currentValue}">${currentValue}</div>`)
+                    .css({top: ((1 + i * 15)+'vmin'), left: ((1 + j * 15)+'vmin')})
+                    .attr('id', (i * size + j))
+                );
+            }
+        }
     }
 }
 
-export const loadNewGame = function(game){
-    for(var i = 0; i < game.board.length; i++){
-        document.getElementById(i.toString(10)).innerHTML = game.board[i];
-        if(game.board[i] == 0){
-            $(`#${i.toString(10)}`).css("visibility", "hidden"); 
-        }else{
-            $(`#${i.toString(10)}`).css("visibility", "visible"); 
-
-        }
-        colorTiles(game, i);
-
+function detachAll() {
+    $(document).off('keydown');
+    let winMessage = $('#win-message');
+    if (winMessage.length) {
+        winMessage.remove();
     }
-    document.getElementById("score").innerHTML = "Score: " + game.score;
-    if(createdListeners == false){
-        $(document).keydown(function(e){
-            var s = String.fromCharCode(e.which);
-            keyPressed(s, game);
-            updateBoard(game);
-        });
-        $('#reset').on("click", function(){
-            game.setupNewGame();
-            document.getElementById("end").innerHTML = "";
-            loadNewGame(game);
-        });
-        createdListeners = true;
-    }
-};
-
-export const colorTiles = function(game, i){
-    switch(game.board[i]){
-        case 2:
-            $(`#${i.toString(10)}`).css("background", "#ffffff");
-            $(`#${i.toString(10)}`).css("color", "black");
-            $(`#${i.toString(10)}`).css("font-size", "2vw");
-            break;
-        case 4:
-            $(`#${i.toString(10)}`).css("background", "#eaeaea");
-            $(`#${i.toString(10)}`).css("color", "black");
-            $(`#${i.toString(10)}`).css("font-size", "2vw");
-            break;
-        case 8:
-            $(`#${i.toString(10)}`).css("background", "#d4d4d4");
-            $(`#${i.toString(10)}`).css("color", "black");
-            $(`#${i.toString(10)}`).css("font-size", "2vw");
-            break;
-        case 16:
-            $(`#${i.toString(10)}`).css("background", "#bfbfbf");
-            $(`#${i.toString(10)}`).css("color", "black");
-            $(`#${i.toString(10)}`).css("font-size", "2vw");
-            break;
-        case 32:
-            $(`#${i.toString(10)}`).css("background", "#aaaaaa");
-            $(`#${i.toString(10)}`).css("color", "black");
-            $(`#${i.toString(10)}`).css("font-size", "2vw");
-            break;
-        case 64:
-            $(`#${i.toString(10)}`).css("background", "#959595");
-            $(`#${i.toString(10)}`).css("color", "black");
-            $(`#${i.toString(10)}`).css("font-size", "2vw");
-            break;
-        case 128:
-            $(`#${i.toString(10)}`).css("background", "#808080");
-            $(`#${i.toString(10)}`).css("color", "white");
-            $(`#${i.toString(10)}`).css("font-size", "2vw");
-            break;
-        case 256:
-            $(`#${i.toString(10)}`).css("background", "#6a6a6a");
-            $(`#${i.toString(10)}`).css("color", "white");
-            $(`#${i.toString(10)}`).css("font-size", "2vw");
-            break;
-        case 512:
-            $(`#${i.toString(10)}`).css("background", "#555555");
-            $(`#${i.toString(10)}`).css("color", "white");
-            $(`#${i.toString(10)}`).css("font-size", "2vw");
-            break;
-        case 1024:
-            $(`#${i.toString(10)}`).css("background", "#404040");
-            $(`#${i.toString(10)}`).css("color", "white");
-            $(`#${i.toString(10)}`).css("font-size", "2vw");
-            break;
-        case 2048:
-            $(`#${i.toString(10)}`).css("background", "#2b2b2b");
-            $(`#${i.toString(10)}`).css("color", "white");
-            $(`#${i.toString(10)}`).css("font-size", "2vw");
-            break;
-        case 4096:
-            $(`#${i.toString(10)}`).css("background", "#151515");
-            $(`#${i.toString(10)}`).css("color", "white");
-            $(`#${i.toString(10)}`).css("font-size", "2vw");
-            break;
-        case 8192:
-            $(`#${i.toString(10)}`).css("background", "#000000");
-            $(`#${i.toString(10)}`).css("color", "white");
-            $(`#${i.toString(10)}`).css("font-size", "2vw");
-            break;
-        default:
-            $(`#${i.toString(10)}`).css("background", "#242431");
-            $(`#${i.toString(10)}`).css("color", "white");
-            $(`#${i.toString(10)}`).css("font-size", "2vw");
+    let loseMessage = $('#lose-message');
+    if (loseMessage.length) {
+        loseMessage.remove();
     }
 }
 
-export const keyPressed = function(key, game){
-    switch (key) {
-        case '\'':
-            game.move('right');
-            break;
-        case '%':
+function attachEventHandlers(game) {
+    $(document).keydown((keyEvent) => {
+        switch(keyEvent.which) {
+            case 37: // left
             game.move('left');
             break;
-        case '(':
-            game.move('down');
-            break;
-        case '&':
+            case 38: // up
             game.move('up');
             break;
-    }
+            case 39: // right
+            game.move('right');
+            break;
+            case 40: // down
+            game.move('down');
+            break;
+        };
+        loadTiles(game);
+        loadScore(game);
+    });
+
+    game.onLose(() => {
+        // alert("You lost!\n\nselect New Game to try again");
+        $('<div id="lose-message"></div>')
+        .appendTo('#root')
+        .append(
+            $(`
+            <div>
+                <p>You lost, press <b>New Game</b> to try again</p>
+            </div>
+            `)
+            .css({  "font-size": "6vmin", "color": "white", 
+                    "text-align": "center", "opacity": "1",
+                    "background-color": "black"})
+        )
+    });
+    game.onWin(() => {
+        // alert("You Win!\n\nKeep going!");
+        if(!($('#root').find('#win-message').length)) {
+            $('<div id="win-message"></div>').appendTo('#root').append(
+                $(`<div>
+                    <p>You won! Your score was: ${game.gameState.score}</p>
+                    <p> Press <b>Continue</b> to keep going <br>
+                        or <b>New Game</b> to start over</p>
+                    <button class="button is-white has-text-weight-semibold" id="continue">Continue</button>
+                </div>`)
+                .css({  "font-size": "6vmin", "color": "white", 
+                        "text-align": "center", "opacity": "0.9", 
+                        "background-color": "black"}))
+
+                .find('#continue').on('click', () => {
+                    $('#win-message').empty();
+                })
+        }
+    });
 }
 
-$(document).ready(function () {
+function reloadGame(game) {
+    detachAll();
+    loadScore(game);
+    loadTiles(game);
+    attachEventHandlers(game);
+}
+
+function initializePage() {
+    let body = $('body');
+    
+    // Load initial empty page
+    body.empty()
+    body.append(gameview());
+
+    // Load game board
     let game = new Game(4);
-    loadNewGame(game);
-});
+    loadGame(game);
+    attachEventHandlers(game);
+
+    // Set up new game button
+    $('#newgame-button').on('click', () => {
+        reloadGame(new Game(4));
+    });
+}
+
+$(document).ready(initializePage());
