2fa61cf5698fb753107e080c00270d5a4a34d114
ronaldogdm <ronaldogarciadem@gmail.com>
2019-11-04 22:07:37 -0500

    a08

--- a08/submission.js
+++ a08/submission.js
@@ -19,7 +19,11 @@
  * @returns  {Object}  The body of the HTTP response.
  */
 export async function fn1() {
-
+    const result = await axios({
+        method: 'get',
+        url: 'https://comp426fa19.cs.unc.edu/a08/heroes',
+    });
+    return result.data;
 };
 
 
@@ -39,7 +43,14 @@ export async function fn1() {
  * @returns  {Number}  The HTTP status code of the response.
  */
 export async function fn2() {
-
+    const result = await axios({
+        method: 'get',
+        url: 'https://comp426fa19.cs.unc.edu/a08/heroes',
+        params: {
+            sort: 'first ASC'
+        },
+    });
+    return result.status;
 };
 
 
@@ -70,7 +81,16 @@ export async function fn2() {
  * @returns  {Object}  The complete axios response object
  */
 export async function fn3() {
-  
+    const result = await axios({
+        method: 'post',
+        url: 'https://comp426fa19.cs.unc.edu/a08/users',
+        params: {
+            first: 'Ketan',
+            last: 'Mayer-Patel',
+            onyen: 'KMP',
+        },
+    });
+    return result.data;
 };
 
 
@@ -92,7 +112,19 @@ export async function fn3() {
  *                     request fails
  */
 export async function fn4() {
-  
+    try {
+        const result = await axios({
+            method: 'post',
+            url: 'https://comp426fa19.cs.unc.edu/a08/users',
+            params: {
+                first: 'Ketan',
+                last: 'Mayer-Patel',
+            },
+        });
+        return result.data;
+    } catch (error) {
+        return error;
+    }
 };
 
 
@@ -110,7 +142,14 @@ export async function fn4() {
  * @returns  {Object}  The complete axios response object
  */
 export async function fn5() {
-  
+    const result = await axios({
+        method: 'put',
+        url: 'https://comp426fa19.cs.unc.edu/a08/headers',
+        headers: {
+            'my-custom-request-header': 'Hello, World!',
+        },
+    });
+    return result.data;
 };
 
 
@@ -131,5 +170,9 @@ export async function fn5() {
  *   "my-custom-response-header"
  */
 export async function fn6() {
-  
+    const result = await axios({
+        method: 'get',
+        url: 'https://comp426fa19.cs.unc.edu/a08/headers',
+    });
+    return result.headers["my-custom-response-header"];
 };
